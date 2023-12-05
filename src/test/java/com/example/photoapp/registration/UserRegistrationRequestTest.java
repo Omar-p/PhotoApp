062b8ff55289c41ee0085b2fed6ead9aeb8ec4ab
Omar Shabaan <omar.plus.077@gmail.com>
2023-12-05 04:28:18 +0200

    add some tests to make sure the execution order of validation annotations in UserRegistrationRequest run as expected.

--- src/test/java/com/example/photoapp/registration/UserRegistrationRequestTest.java
+++ src/test/java/com/example/photoapp/registration/UserRegistrationRequestTest.java
@@ -1,24 +1,45 @@
 package com.example.photoapp.registration;
 
+import com.example.photoapp.UserRepository;
+import com.example.photoapp.UserSecurityRepository;
+import com.example.photoapp.domain.User;
+import com.example.photoapp.domain.UserSecurity;
 import com.example.photoapp.validation.CompleteRegistrationValidation;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import java.util.Optional;
 import java.util.Set;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
+/**
+ * This class serves as a test suite for the execution order of validation annotations to ensure that it runs as expected.
+ * It focuses on the validation of the {@link UserRegistrationRequest} class using the Spring Boot testing framework.
+ * The purpose of these tests is to verify the behavior of the validation process under different scenarios.
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = {
     ValidationAutoConfiguration.class
 })
 class UserRegistrationRequestTest {
 
   private Validator validator;
+
+  @MockBean
+  UserRepository userRepository;
+
+  @MockBean
+  UserSecurityRepository userSecurityRepository;
 
   @Autowired
   LocalValidatorFactoryBean localValidatorFactoryBean;
@@ -43,7 +64,14 @@ class UserRegistrationRequestTest {
         validRequest.confirmPassword()
     );
 
+
+    BDDMockito.willReturn(Optional.empty()).given(userRepository).findByUsername(anyString());
+    BDDMockito.willReturn(Optional.empty()).given(userSecurityRepository).findByEmail(anyString());
+
     Set<ConstraintViolation<UserRegistrationRequest>> constraintViolations = validator.validate(request, CompleteRegistrationValidation.class);
+
+    verify(userRepository, times(1)).findByUsername(anyString());
+    verify(userSecurityRepository, times(1)).findByEmail(anyString());
 
     BDDAssertions.then(constraintViolations.size()).isEqualTo(0);
   }
@@ -60,6 +88,45 @@ class UserRegistrationRequestTest {
     Set<ConstraintViolation<UserRegistrationRequest>> constraintViolations = validator.validate(request, CompleteRegistrationValidation.class);
 
     BDDAssertions.then(constraintViolations.size()).isEqualTo(1);
+  }
+
+  @Test
+  void newRequestWithBasicConstraintViolationShouldNotTouchTheDatabase() {
+    var request = new UserRegistrationRequest(
+        validRequest.username(),
+        validRequest.email(),
+        validRequest.password(),
+        "12345678"
+    );
+
+    Set<ConstraintViolation<UserRegistrationRequest>> constraintViolations = validator.validate(request, CompleteRegistrationValidation.class);
+
+    // verify that the database is not touched
+    verify(userRepository, never()).findByUsername(anyString());
+    verify(userSecurityRepository, never()).findByEmail(anyString());
+
+    BDDAssertions.then(constraintViolations.size()).isEqualTo(1);
+  }
+
+  @Test
+  void newRequestWithTakenEmailAndUsernameShouldProduceTwoConstraintViolation() {
+    var request = new UserRegistrationRequest(
+        validRequest.username(),
+        validRequest.email(),
+        validRequest.password(),
+        validRequest.confirmPassword()
+    );
+
+    BDDMockito.willReturn(Optional.of(Optional.of(new User()))).given(userRepository).findByUsername(anyString());
+    BDDMockito.willReturn(Optional.of(Optional.of(new UserSecurity()))).given(userSecurityRepository).findByEmail(anyString());
+
+    Set<ConstraintViolation<UserRegistrationRequest>> constraintViolations = validator.validate(request, CompleteRegistrationValidation.class);
+
+    verify(userRepository, times(1)).findByUsername(anyString());
+    verify(userSecurityRepository, times(1)).findByEmail(anyString());
+
+    BDDAssertions.then(constraintViolations.size()).isEqualTo(2);
+
   }
 
 
